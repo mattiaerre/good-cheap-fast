b63c44eb472c8f5833df0a70ea6e2f73df06d8d9
mattia richetto <mattia.richetto@gmail.com>
2019-06-23 22:03:01 -0700

    ready to TDD

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # good-cheap-fast
+
 Good/Cheap/Fast  -  pick two
+
+An experiment w/ React and TDD
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Switch from 'react-ios-switch';
+import './App.css';
+
+function App() {
+  return (
+    <section>
+      <h2>Good/Cheap/Fast - pick two</h2>
+      <form>
+        <label>
+          Good
+          <Switch
+            checked={true}
+            className="label__switch"
+            onChange={checked => {}}
+          />
+        </label>
+        <label>
+          Cheap
+          <Switch
+            checked={false}
+            className="label__switch"
+            onChange={checked => {}}
+          />
+        </label>
+        <label>
+          Fast
+          <Switch
+            checked={false}
+            className="label__switch"
+            onChange={checked => {}}
+          />
+        </label>
+      </form>
+      <h3>What are your priorities?</h3>
+      <ul>
+        <li>Cheap + fast = lower quality work</li>
+        <li>Fast + good = expensive</li>
+        <li>Good + cheap = not happening anytime soon</li>
+      </ul>
+    </section>
+  );
+}
+
+export default App;
