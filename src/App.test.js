231e76583d447325f62b9261ac9bf8416de9d7b3
mattia richetto <mattia.richetto@gmail.com>
2021-09-17 17:45:28 -0400

    add RTL

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders h3', () => {
+  render(<App />);
+  expect(screen.getByText('What are your priorities?')).toBeInTheDocument();
 });
