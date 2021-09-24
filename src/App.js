bd9cf498b88c68fcd511418555b6873e6b6b0e96
Martin Cartledge <mcartledge@opentable.com>
2021-09-24 16:17:29 -0600

    [good-fast-cheap] write logic and wire up UI

--- src/App.js
+++ src/App.js
@@ -1,36 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Switch from 'react-ios-switch';
 import './App.css';
 
+const TYPES = {
+  GOOD: 'good',
+  CHEAP: 'cheap',
+  FAST: 'fast'
+};
+
+function flipSwitch(switchState, activeSwitch) {
+  const switchStateCopy = { ...switchState };
+  switchStateCopy[activeSwitch] = !switchStateCopy[activeSwitch];
+  const { good, cheap, fast } = switchStateCopy;
+  const conditionMet = good && cheap && fast;
+  if (conditionMet) {
+    if (activeSwitch === TYPES.GOOD) {
+      return { cheap: false, good: true, fast: true };
+    }
+    if (activeSwitch === TYPES.CHEAP) {
+      return { cheap: true, good: false, fast: true };
+    }
+    if (activeSwitch === TYPES.FAST) {
+      return { cheap: true, good: false, fast: true };
+    }
+  }
+  return switchStateCopy;
+}
+
 function App() {
+  const initialState = {
+    good: false,
+    cheap: false,
+    fast: false
+  };
+
+  const [switchesState, setSwitches] = useState(initialState);
+
+  const switchData = [
+    {
+      name: 'Good',
+      label: 'good',
+      onChange: flipSwitch,
+      value: switchesState.good
+    },
+    {
+      name: 'Cheap',
+      label: 'cheap',
+      onChange: flipSwitch,
+      value: switchesState.cheap
+    },
+    {
+      name: 'Fast',
+      label: 'fast',
+      onChange: flipSwitch,
+      value: switchesState.fast
+    }
+  ];
+
   return (
     <section>
       <h2>Good/Cheap/Fast - pick two</h2>
       <form>
-        <label>
-          Good
-          <Switch
-            checked={true}
-            className="label__switch"
-            onChange={() => {}}
-          />
-        </label>
-        <label>
-          Cheap
-          <Switch
-            checked={false}
-            className="label__switch"
-            onChange={() => {}}
-          />
-        </label>
-        <label>
-          Fast
-          <Switch
-            checked={false}
-            className="label__switch"
-            onChange={() => {}}
-          />
-        </label>
+        {switchData.map(({ name, label, onChange, value }) => (
+          <label>
+            {name}
+            <Switch
+              checked={value}
+              className="label__switch"
+              onChange={() => setSwitches(onChange(switchesState, label))}
+            />
+          </label>
+        ))}
       </form>
       <h3>What are your priorities?</h3>
       <ul>
